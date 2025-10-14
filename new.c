bcb23c9db3e680857850abdc1252f101018b5184
uki <148555322+Starrywawah@users.noreply.github.com>
2025-10-14 18:44:27 +0800

    Add files via upload

---
+++ new.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/*
+#include <stdio.h> => input/output
+#include <math.h> => values
+#include <string.h> => string handling
+#define constant 1 => form a constant or variable
+#include <windows.h>
+*/
+
+int main(){//function
+
+    printf("hell nah"); //output print in words
+
+    return 0; //end point
+} //brackets
+
+int main(void){ //the newer gen that we usually use now
+
+    printf("hell yea");
+}
+
+//variables
+int a = 2; //interger
+float b = 2.3; //decimals
+//%d is for printing interger
+//$f is for printing float
+scanf()
+
+char c = 'alamak'//character, not work for calc bcs binary
+//%c is for printing char
+
+/*char in calculation will appear as ASCII character bcs 
+the object is used for a binary number */
+
+char name[] = "mizuki"; //string or array
+//%s is for string in printf
